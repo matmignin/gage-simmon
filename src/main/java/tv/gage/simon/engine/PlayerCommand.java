abbcfc9877912381e06ee5d2f47a48601e86fa1f
aljunggren <aljunggren@allegient.com>
2020-01-04 19:30:45 -0500

    Initial commit

---
+++ src/main/java/tv/gage/simon/engine/PlayerCommand.java
@@ -0,0 +1,21 @@
+package tv.gage.simon.engine;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerCommand {
+
+	public static enum CommandType {
+		MOVE
+	}
+	
+	private CommandType type;
+	private String data;
+	
+}
